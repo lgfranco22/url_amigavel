404261eab47770bd1ba7e887e49f1c1e07996c42
lgfranco22 <39537987+lgfranco22@users.noreply.github.com>
2021-02-24 18:09:14 -0300

    add header php

---
+++ views/header.php
@@ -0,0 +1,9 @@
+<header>
+<ul>
+<li><a href="home"></a></li>
+<li><a href="blog"></a></li>
+<li><a href="sobre"></a></li>
+<li><a href="contato"></a></li>
+<li><a href="erro"></a></li>
+</ul>
+</header>
